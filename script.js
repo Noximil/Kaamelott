87761da116ff96a3a4fafed354d2dda6a5b328fd
Noximil <titouanhugel@icloud.com>
2025-04-14 21:03:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ let currentIndex = -1;
 
 async function fetchQuotes() {
   try {
-    const response = await fetch("https://script.google.com/macros/s/AKfycbzqucA3BaaLsJkZzlFYydgCXWiqS5KvNuElsQK30AOtnR-kzOIKJGudF3jT8obXLPBeog/exec");
+    const response = await fetch("https://script.google.com/macros/s/AKfycbyWk0LW0E9vqJhtgWF2zr2Bb5rRsYCS5B4-EY-LrMM6k5_hhxr0PORG2j6camUge_WEQQ/exec");
     quotes = await response.json();
     filteredQuotes = [...quotes];
     showNewQuote();
@@ -99,7 +99,7 @@ document.getElementById("quote-form").addEventListener("submit", async function 
   };
 
   try {
-    const res = await fetch("https://script.google.com/macros/s/AKfycbzqucA3BaaLsJkZzlFYydgCXWiqS5KvNuElsQK30AOtnR-kzOIKJGudF3jT8obXLPBeog/exec", {
+    const res = await fetch("https://script.google.com/macros/s/AKfycbyWk0LW0E9vqJhtgWF2zr2Bb5rRsYCS5B4-EY-LrMM6k5_hhxr0PORG2j6camUge_WEQQ/exec", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(citation)
